bee168e280e8aebde50e8e1c1add5784cde790a1
ashish kumar <ashishkumar7249@gmail.com>
2018-02-18 13:25:15 +0530

    multi sticker functionality added

--- StickyNotesJs/stickyNotesJs.js
+++ StickyNotesJs/stickyNotesJs.js
@@ -2,6 +2,7 @@
 //			2.When you place sticker out of browser window,it gets lost.
 //			3.When you increase size of sticker,then on dragging,it is losing opacity.
 //			4.It cannot be increased in size from anydirection.
+var container;
 function getCordinates(){
 	var arrCor = [];
 	arrCor[0] = event.clientX;
@@ -14,11 +15,12 @@ function updateLocation(){
 	arr =getCordinates();
 	corX = arr[0];
 	corY = arr[1];
-	$('#container').css({"left": corX,"top":corY});
+	$(container).css({"left": corX,"top":corY});
 }
 
 function dragging(event) {
-	$('#container').hide();			//Instead of dragging event,I thought  of using ondragstart event,but I found problem that container 
+	$(event.target.id).hide();	
+	container = "#"+event.target.id;		//Instead of dragging event,I thought  of using ondragstart event,but I found problem that container 
 }									//became hidden,once I start dragging,and I lost even the copy of dragging.If I had not used drag(dragging function) event
 									//I would not have done this project.Thanks to God.
 
@@ -29,5 +31,21 @@ function allowDrop(event) {
 
 function drop(event) {
 	updateLocation();				//I thought that if I would write event.preventDefault(); then,drop would not take place,but 
-	$('#container').show();			//this is not the case.Writing event.preventDefault(); has no effect here.
+	$(container).show();			//this is not the case.Writing event.preventDefault(); has no effect here.
 }
+
+var index = 1;
+var color = ['pink','DeepSkyBlue','BurlyWood'];
+var top = ['100px','120px','140px'];
+var left = ['100px','120px','140px'];
+
+$(document).ready(function(){
+	$('#addNotes').click(function(){
+		var text = 	`<textarea  draggable="true"   id="index${index}"  ondrag="dragging(event)"  style = "height:200px;width:200px;border:solid;position:absolute;"></textarea>`;
+		$('body').append(text);
+		var selector = `#index${index}`;
+		$(selector).css({'background-color':color[index%3],top:top[index%3],left:left[index%3]});
+		index++;
+	});
+});
+
